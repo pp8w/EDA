17d8af7f99b8ba3e710dad9e63e345721d12776b
pp8w <philip.nmn.park@gmail.com>
2016-07-21 12:57:14 -0400

    Titanic.R

---
+++ Titanic.R
@@ -0,0 +1,27 @@
+#This is the code from the Titanic exercise in ggplot
+#Other than these intro lines, it is pasted as is
+#As such, certain packages and dataframes are already
+#assumed to be in workspace
+#----------------------------------------------------
+# titanic is avaliable in your workspace
+
+# Check out the structure of titanic
+str(titanic)
+
+# Use ggplot() for the first instruction
+ggplot(titanic, aes(x=factor(Pclass), fill=factor(Sex))) + 
+geom_bar(position = "dodge")
+
+
+# Use ggplot() for the second instruction
+ggplot(titanic, aes(x=factor(Pclass), fill=factor(Sex))) + 
+geom_bar(position = "dodge") + facet_grid(". ~ Survived")
+
+
+# Position jitter (use below)
+posn.j <- position_jitter(0.5, 0)
+
+# Use ggplot() for the last instruction
+ggplot(titanic, aes(x=factor(Pclass),y=Age, col=factor(Sex))) + 
+geom_jitter(size = 3, alpha = 0.5, position = posn.j) + 
+facet_grid(". ~ Survived")
